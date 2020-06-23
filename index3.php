5455b43c6e25a41a950b06948d6d3c4993200fb1
Ankit <ankitraiiv@gmail.com>
2020-06-23 21:34:18 +0530

    ...

---
+++ index3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>phpDocument</title>
+</head>
+<body>
+    <?php
+    $car=array("honda","mercedez","lamborgini","rollce royce");
+    // foreach($car as $value){
+    //     echo "$value <br>";
+    for($x=0;$x<4;$x++){
+        echo "i wnt to drive".$car[$x];
+        echo "<br>";
+    }
+    
+
+
+    ?>
+</body>
+</html>
